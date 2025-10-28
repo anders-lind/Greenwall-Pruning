b8b0b53d982ec5fe64ade28adc0b7f852cf2f528
Anders Lind-Thomsen <anders@lind-thomsen.dk>
2025-10-28 10:46:20 +0100

    added read to DynamixelSync

--- libs/DynamixelSync.py
+++ libs/DynamixelSync.py
@@ -3,9 +3,10 @@ from enum import Enum
 
 class CONTROL_ADDRESS(Enum):
     # NAME = (ADDRESS, LEN)
-    GOAL_VELOCITY = (104, 4)
     TORQUE_ENABLE = (64, 1)
     LED = (65, 1)
+    GOAL_VELOCITY = (104, 4)
+    PRESENT_POSITION = (132, 4)
 
 
 class DynamixelSync:
@@ -15,22 +16,41 @@ class DynamixelSync:
         self.port_handler.openPort()
         self.port_handler.setBaudRate(baudrate=buad_rate)
 
-        self.motor_num_to_id_list = [0,1,2,3]
 
-
-
-    def write(self, motors: list[int], values: list, control_type: CONTROL_ADDRESS):
-        groupSyncWrite = dxl.GroupSyncWrite(self.port_handler, self.packet_handler, control_type.value[0], control_type.value[1])
+    def write(self, motors: list[int], values: list, control_type: CONTROL_ADDRESS) -> None:
+        group_sync_write = dxl.GroupSyncWrite(self.port_handler, self.packet_handler, control_type.value[0], control_type.value[1])
         
         for i in range(len(motors)):
-            motor_id = self.motor_to_motor_id(motors[i])
+            motor_id = self.motor_name_to_motor_id(motors[i])
             param = (values[i]).to_bytes(control_type.value[1], 'little', signed=True)
-            groupSyncWrite.addParam(motor_id, param)
+            group_sync_write.addParam(motor_id, param)
         
-        groupSyncWrite.txPacket()
+        group_sync_write.txPacket()
+    
+
+    def read(self, motors: list[int], control_type: CONTROL_ADDRESS) -> list:
+        group_sync_read = dxl.GroupSyncRead(self.port_handler, self.packet_handler, control_type.value[0], control_type.value[1])
+        values = []
+
+        for i in range(len(motors)):
+            motor_id = self.motor_name_to_motor_id(motors[i])
+            res = group_sync_read.addParam(motor_id)
+            if res != True:
+                print("ERROR: Invalid read param motor ID: " + str(motor_id))
+                raise KeyError
+
+        group_sync_read.txRxPacket()
+
+        for i in range(len(motors)):
+            motor_id = self.motor_name_to_motor_id(motors[i])
+            value = group_sync_read.getData(motor_id, control_type.value[0], control_type.value[1])
+            values.append(value)
+
+        return values
 
 
-    def enable_torque(self, motors):
+
+    def enable_torque(self, motors) -> None:
         values = []
         for i in range(len(motors)):
             values.append(1)
@@ -38,12 +58,41 @@ class DynamixelSync:
         self.write(motors=motors, values=values, control_type=CONTROL_ADDRESS.TORQUE_ENABLE)
 
 
-    def motor_to_motor_id(self, motor_num: int) -> int:
-        return self.motor_num_to_id_list[motor_num]
+    def disable_torque(self, motors) -> None:
+        values = []
+        for i in range(len(motors)):
+            values.append(0)
+
+        self.write(motors=motors, values=values, control_type=CONTROL_ADDRESS.TORQUE_ENABLE)
+
+
+    def motor_name_to_motor_id(self, motor_num: int) -> int:
+        if motor_num == 1:
+            return 1
+        if motor_num == 2:
+            return 2
+        if motor_num == 3:
+            return 3
+        if motor_num == 4:
+            return 4
+        
+        print("ERROR: Motor number has no accociated ID")
+        raise ValueError
 
 
 if __name__  == "__main__":
     dmx = DynamixelSync()
     motors = [2]
+    
     dmx.enable_torque(motors)
-    dmx.write(motors, [000], CONTROL_ADDRESS.GOAL_VELOCITY)
+    dmx.disable_torque(motors)
+    dmx.write(motors, [-10], CONTROL_ADDRESS.GOAL_VELOCITY)
+
+    values = dmx.read(motors, CONTROL_ADDRESS.PRESENT_POSITION)
+    print(values)
+    print(type(values[0]))
+    
+
+
+    # test read
+    # test disable torque
