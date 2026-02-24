8fc117d8c4e010547edba7bb7eabce703ac5ea7e
alexellegaard <alexellegaard@gmail.com>
2026-02-24 12:29:03 +0100

    added yet another point for workspace test

--- src/cdpr_control_pkg/cdpr_control_pkg/cdpr_pathplanner.py
+++ src/cdpr_control_pkg/cdpr_control_pkg/cdpr_pathplanner.py
@@ -36,15 +36,15 @@ class CDPRPathplannerNode(Node):
         # ]
 
         # Workspace test poses
-        self.pos = [self.center_pos, np.array([self.CDPR_width, self.CDPR_height])] # top right corner
-        # self.pos = [self.center_pos, np.array([self.CDPR_width, 0.0])] # bottom right corner
-        # self.pos = [self.center_pos, np.array([0.0, 0.0])] # bottom left corner
-        # self.pos = [self.center_pos, np.array([0.0, self.CDPR_height])] # top left corner
+        self.pos = [self.clear_homing_stick, self.center_pos, np.array([self.CDPR_width, self.CDPR_height])] # top right corner
+        # self.pos = [self.clear_homing_stick, self.center_pos, np.array([self.CDPR_width, 0.0])] # bottom right corner
+        # self.pos = [self.clear_homing_stick, self.center_pos, np.array([0.0, 0.0])] # bottom left corner
+        # self.pos = [self.clear_homing_stick, self.center_pos, np.array([0.0, self.CDPR_height])] # top left corner
 
-        # self.pos = [self.center_pos, np.array([self.CDPR_width, self.CDPR_height/2])] # right side
-        # self.pos = [self.center_pos, np.array([self.CDPR_width/2, 0.0])] # bottom side
-        # self.pos = [self.center_pos, np.array([0.0, self.CDPR_height/2])] # left side
-        # self.pos = [self.center_pos, np.array([self.CDPR_width/2, self.CDPR_height])] # top side
+        # self.pos = [self.clear_homing_stick, self.center_pos, np.array([self.CDPR_width, self.CDPR_height/2])] # right side
+        # self.pos = [self.clear_homing_stick, self.center_pos, np.array([self.CDPR_width/2, 0.0])] # bottom side
+        # self.pos = [self.clear_homing_stick, self.center_pos, np.array([0.0, self.CDPR_height/2])] # left side
+        # self.pos = [self.clear_homing_stick, self.center_pos, np.array([self.CDPR_width/2, self.CDPR_height])] # top side
 
         self.pathplanner_loop_period = 0.02
 
