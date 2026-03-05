365abe40a6706cdd6c7cdf85b1f3cacbef2be231
Anders Lind-Thomsen <anders@lind-thomsen.dk>
2026-03-05 13:57:42 +0100

    added include and better build

--- src/leaf_detection_pkg/leaf_detection_pkg/leaf_detection.py
+++ src/leaf_detection_pkg/leaf_detection_pkg/leaf_detection.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import matplotlib.pyplot as plt
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image # Import the Image message type
@@ -7,12 +8,11 @@ from plantwall_custom_interfaces.msg import CdprPose
 import torch
 import numpy as np
 from cv_bridge import CvBridge
-import matplotlib.pyplot as plt
 import os
 import cv2
 
 from sam2.build_sam import build_sam2
-
+# 
 class LeafDetectionNode(Node):
     def __init__(self):
         super().__init__('leaf_detection')
