7d5702a4e5c52beb602f13e2f0ee9118be81ea6b
alexellegaard <alexellegaard@gmail.com>
2026-03-06 10:28:26 +0100

    implemented sam2 in leaf detection node

--- src/leaf_detection_pkg/leaf_detection_pkg/leaf_detection.py
+++ src/leaf_detection_pkg/leaf_detection_pkg/leaf_detection.py
@@ -12,6 +12,7 @@ import os
 import cv2
 
 from sam2.build_sam import build_sam2
+from sam2.sam2_image_predictor import SAM2ImagePredictor
 
 class LeafDetectionNode(Node):
     def __init__(self):
@@ -32,11 +33,19 @@ class LeafDetectionNode(Node):
 
         # Load training
         # mahal_data_path = "/home/anders/workspace/masters_thesis/Greenwall-Pruning/test_scripts/perception/perception_stats_cielab.npy"
-        mahal_data_path = "/home/anders/workspace/masters_thesis/Greenwall-Pruning/test_scripts/perception/perception_stats.npy"
+        mahal_data_path = "/home/alex/Thesis/Greenwall-Pruning/test_scripts/perception/perception_stats.npy"
         if not os.path.exists(mahal_data_path):
             print(f"Error: {mahal_data_path} not found. Please run your training script first.")
             exit()
         self.mahal_data = np.load(mahal_data_path, allow_pickle=True).item()
+
+        home = os.path.expanduser("~")
+        sam2_checkpoint = os.path.join(home, "/home/alex/Thesis/sam2/checkpoints/sam2.1_hiera_base_plus.pt")
+        model_cfg = "sam2_hiera_b+.yaml"
+        sam2_model = build_sam2(model_cfg, ckpt_path=None, device=self.device)
+        sd = torch.load(sam2_checkpoint, map_location=self.device, weights_only=True)["model"]
+        sam2_model.load_state_dict(sd, strict=False)
+        self.predictor = SAM2ImagePredictor(sam2_model)
 
         # Subscriber for Color Image
         self.realsense_color_subscriber = self.create_subscription(
@@ -48,22 +57,16 @@ class LeafDetectionNode(Node):
         # Subscriber for Depth Image
         self.realsense_depth_subscriber = self.create_subscription(
             Image, 
-            '/camera/camera/depth/image_rect_raw', 
+            '/camera/camera/aligned_depth_to_color/image_raw', 
             self.depth_image_callback, 
             10)
 
 
-    def color_image_callback(self, msg):
-        # 'msg' is a sensor_msgs/Image object
-        # self.get_logger().info(f"Received color image: {msg.width}x{msg.height}", throttle_duration_sec=0.2)
-        
+    def color_image_callback(self, msg):    
         self.color_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding='rgb8')        
 
 
     def depth_image_callback(self, msg):
-        # 'msg' is a sensor_msgs/Image object (often 16-bit integers for depth)
-        # self.get_logger().info(f"Received depth image: {msg.width}x{msg.height}", throttle_duration_sec=0.2)
-        
         self.depth_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding='16UC1')
         
 
@@ -79,11 +82,18 @@ class LeafDetectionNode(Node):
             print("no leaf coordinate found")
             return
 
-        self.get_logger().info(f"Found leaf at coordinate: {leaf_coordinate}")
+        self.get_logger().info(f"Mahalanobis candidate leaf found at coordinate: {leaf_coordinate}")
 
         # If possible leaf found, use SAM2 on point
         segment_mask = self.run_sam2(leaf_coordinate)
 
+        # Apply mask on self.color_image for visualization
+        masked_img = cv2.bitwise_and(self.color_image, self.color_image, mask=segment_mask.astype(np.uint8))
+        # # show masked image
+        # plt.imshow(masked_img)
+        # plt.show()
+
+        
 
     def mahalanobis_check(self) -> np.ndarray|None:
         class_configs = {
@@ -99,16 +109,13 @@ class LeafDetectionNode(Node):
 
         # Get copy of image
         img = self.color_image.copy()
-        
-        # Convert to desired color space
-        # img = cv2.cvtColor(img, cv2.COLOR_RGB2Lab).astype(float)
 
         # Get image information
         rows, cols, _ = img.shape
         pixels = img.reshape(-1, 3)
         combined_morphed = np.zeros((rows, cols), dtype=np.uint8)
 
-        # Created maha masks for each leaf type
+        # Created Mahalanobis masks for each leaf class
         for i, (cls, config) in enumerate(class_configs.items()):
             mu = self.mahal_data[cls]["mean"]
             inv_cov = self.mahal_data[cls]["inv_cov"]
@@ -122,37 +129,23 @@ class LeafDetectionNode(Node):
             
             # Combine morphed images
             combined_morphed = cv2.bitwise_or(combined_morphed, morphed)
-
-            plt.subplot(2, 2, i+1)
-            plt.imshow(morphed, cmap='gray')
-            plt.waitforbuttonpress(timeout=0.01)
-            plt.title(f"Segmentation: {cls.upper()}")
-            plt.axis('off')
         
-
-        # Find biggest contour
+        # Find largest contour
         contours,_ = cv2.findContours(combined_morphed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         if not contours:
             print("No contours found!")
             return None
         largest_contour = max(contours, key=cv2.contourArea)
 
-        # Check if contour is big enough
+        # Check if largest contour is big enough
         if cv2.contourArea(largest_contour) < self.contour_area_threshold:
             self.get_logger().info("Mahal found no leaf big enough")
             return None
         
         biggest_leaf_mask = np.zeros_like(combined_morphed)
         cv2.drawContours(biggest_leaf_mask, [largest_contour], -1, 255, -1)
-
-        plt.subplot(2, 2, 3)
-        plt.imshow(biggest_leaf_mask, cmap='gray')
-        plt.title("biggest_leaf_mask")
-        
+    
         dist_trans = cv2.distanceTransform(biggest_leaf_mask, cv2.DIST_L2, 5)
-        plt.subplot(2, 2, 4)
-        plt.imshow(dist_trans)
-        plt.title("dist_trans")
         _, _, _, max_loc = cv2.minMaxLoc(dist_trans)
         seed_point = np.array([[max_loc[0], max_loc[1]]])
         print(f"Seed point: {seed_point}")
@@ -161,8 +154,16 @@ class LeafDetectionNode(Node):
 
 
     def run_sam2(self, leaf_coordinate):
-        pass
-
+        with torch.inference_mode(), torch.autocast(device_type=self.device, dtype=torch.bfloat16):
+            self.predictor.set_image(self.color_image)
+            masks, scores, _ = self.predictor.predict(
+                point_coords=leaf_coordinate,
+                point_labels=np.array([1]), 
+                multimask_output=True,
+            )
+        
+        best_mask = masks[np.argmax(scores)]
+        return best_mask
 
 def main(args=None):
     rclpy.init(args=args)
