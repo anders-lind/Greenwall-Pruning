a5d4510cb7aa038426c0204344ba83826ed287d4
alexellegaard <alexellegaard@gmail.com>
2026-04-20 15:48:47 +0200

    made dynamixel_driver_pkg

---
+++ src/dynamixel_driver_pkg/dynamixel_driver_pkg/DynamixelSync.py
@@ -0,0 +1,196 @@
+from dynamixel_sdk import PortHandler, PacketHandler, GroupSyncWrite, GroupSyncRead
+from enum import Enum, IntEnum
+
+class CONTROL_TABLE(Enum):
+    # NAME = (ADDRESS, LEN, IS_SIGNED)
+    DRIVE_MODE = (10, 1, False)
+    OPERATING_MODE = (11, 1, False)
+    PROTOCOL_TYPE = (13, 1, False)
+    HOMING_OFFSET = (20, 4, True)
+    MAX_VOLTAGE_LIMIT = (32, 2, False)
+    MIN_VOLTAGE_LIMIT = (34, 2, False)
+    CURRENT_LIMIT = (38, 2, False)
+    VELOCITY_LIMIT = (44, 4, False)
+    TORQUE_ENABLE = (64, 1, False)
+    LED = (65, 1, False)
+    GOAL_CURRENT = (102, 2, True)
+    GOAL_VELOCITY = (104, 4, True)
+    PROFILE_ACCELERATION = (108, 4, False)
+    PROFILE_VELOCITY = (112, 4, False)
+    GOAL_POSITION = (116, 4, True)
+    MOVING = (122, 1, False)
+    PRESENT_LOAD = (126, 2, True)
+    PRESENT_CURRENT = (126, 2, True)
+    PRESENT_VELOCITY = (128, 4, True)
+    PRESENT_POSITION = (132, 4, True)
+
+
+class OPERATING_MODES(IntEnum):
+    CURRENT_CONTROL_MODE = 0
+    VELOCITY_CONTROL_MODE = 1
+    POSITION_CONTROL_MODE = 3
+    EXTENDED_POSITION_CONTROL_MODE = 4
+    CURRENT_BASSED_POSITION_CONTROL_MODE = 5
+    PWN_CONTROL_MODE = 16
+
+
+class DynamixelSync:
+    def __init__(self, port_name: str = "/dev/ttyUSB0", buad_rate: int = 115200, protocol_version: float = 2.0) -> None:
+        self.port_handler = PortHandler(port_name=port_name)
+        self.packet_handler = PacketHandler(protocol_version=protocol_version)
+        self.port_handler.openPort()
+        self.port_handler.setBaudRate(baudrate=buad_rate)
+        self.motorDirections = {1:1, 2:1, 3:1, 4:1, 11:1, 12:1}
+
+    
+    def __del__(self):
+        print("Dynamixel destructor")
+        self.disable_torque(motors=[1,2,3,4,11,12])
+    
+
+    def setTurningDirection(self, motors: list[int], directions: list[int]) -> None:
+        # Check if direction contain invalid values (anything other than -1 and 1)
+        for i in range(len(directions)):
+            if not (directions[i] == -1 or directions[i] == 1):
+                print(f"Warning: Turning direction contains the invalid number \"{directions[i]}\"! Only -1 and 1 allowed.")
+                if directions[i] >= 0:
+                    directions[i] = 1
+                    print(f"Warning: Turning direction have been automatically set to 1")
+                else:
+                    directions[i] = -1
+                    print(f"Warning: Turning direction have been automatically set to -1")
+
+        for i in range(len(motors)):
+            self.motorDirections[motors[i]] = directions[i]
+
+
+    def write(self, motors: list[int], values: int|list[int]|OPERATING_MODES, control_type: CONTROL_TABLE) -> None:
+        # Check if values is a list only containing integers
+        if isinstance(values, list):
+            for i in range(len(values)):
+                if not isinstance(values[i], int):
+                    print(f"ERROR: 'values' contain non-integer value \"{values[i]}\" at index={i}.")
+                    raise TypeError
+        # Check if values is integer
+        elif not isinstance(values, int):
+            print(f"ERROR: 'values' not an integer or a list of integers! Is type \"{type(values)}\"")
+            raise TypeError
+
+            
+        
+        address = control_type.value[0]
+        data_length = control_type.value[1]
+        is_signed = control_type.value[2]
+
+        group_sync_write = GroupSyncWrite(self.port_handler, self.packet_handler, address, data_length)
+
+        # If signed, adjust values based on motor direction
+        if is_signed:
+            if isinstance(values, int):
+                values = values * self.motorDirections[motors[0]]
+            elif isinstance(values, list):
+                for i in range(len(values)):
+                    values[i] = values[i] * self.motorDirections[motors[i]]
+
+        for i in range(len(motors)):
+            motor_id = self.motor_name_to_motor_id(motors[i])
+            param = None
+            if isinstance(values, int):
+                try:
+                    param = (values).to_bytes(data_length, 'little', signed=is_signed)
+                except:
+                    print("ERROR: (values, data_len)", values, ",", data_length)
+            elif isinstance(values, list):
+                try:
+                    param = (values[i]).to_bytes(data_length, 'little', signed=is_signed)
+                except:
+                    print("ERROR: (values[i], data_len)", values[i], ",", data_length)
+            elif isinstance(values, OPERATING_MODES):
+                param = (values.value).to_bytes(data_length, 'little', signed=is_signed)
+            else:
+                print("ERROR: invalid values type. Got:", type(values))
+            group_sync_write.addParam(motor_id, param)
+        
+        success = group_sync_write.txPacket()
+        if success != 0:
+            print("ERROR: Could not write! Got result:", success)
+    
+
+    def read(self, motors: list[int], control_type: CONTROL_TABLE) -> list:
+        address = control_type.value[0]
+        data_length = control_type.value[1]
+        is_signed = control_type.value[2]
+
+        group_sync_read = GroupSyncRead(self.port_handler, self.packet_handler, address, data_length)
+        values = []
+
+        for i in range(len(motors)):
+            motor_id = self.motor_name_to_motor_id(motors[i])
+            res = group_sync_read.addParam(motor_id)
+            if res != True:
+                print("ERROR: Invalid read param motor ID: " + str(motor_id))
+                raise KeyError
+
+        success = group_sync_read.txRxPacket()
+        if success != 0:
+            print("ERROR: Could not read. Got result:", success)
+            for i in range(len(motors)):
+                values.append(None)
+            return values
+
+
+        for i in range(len(motors)):
+            motor_id = self.motor_name_to_motor_id(motors[i])
+            value = group_sync_read.getData(motor_id, control_type.value[0], control_type.value[1])
+            
+            if is_signed:
+                bit_length = data_length * 8
+                sign_bit_mask = 1 << (bit_length - 1)
+                if value & sign_bit_mask:
+                    value = value - (1 << bit_length)
+
+            values.append(value)
+        
+        # If signed, adjust values based on motor direction
+        if is_signed:
+            for i in range(len(values)):
+                values[i] = values[i] * self.motorDirections[motors[i]]
+
+        return values
+
+
+
+    def enable_torque(self, motors) -> None:
+        values = []
+        for i in range(len(motors)):
+            values.append(1)
+
+        self.write(motors=motors, values=values, control_type=CONTROL_TABLE.TORQUE_ENABLE)
+
+
+    def disable_torque(self, motors) -> None:
+        values = []
+        for i in range(len(motors)):
+            values.append(0)
+
+        self.write(motors=motors, values=values, control_type=CONTROL_TABLE.TORQUE_ENABLE)
+
+
+    def motor_name_to_motor_id(self, motor_num: int) -> int:
+        return motor_num
+        
+        print("ERROR: Motor number has no accociated ID")
+        raise ValueError
+
+
+if __name__  == "__main__":
+    dmx = DynamixelSync()
+    motors = [2]
+    
+    dmx.enable_torque(motors)
+    # dmx.disable_torque(motors)
+    dmx.write(motors, [-10], CONTROL_TABLE.GOAL_VELOCITY)
+
+    # for i in range(1000):
+    values = dmx.read(motors, CONTROL_TABLE.PRESENT_POSITION)
+    print(values)
---
+++ src/dynamixel_driver_pkg/dynamixel_driver_pkg/dynamixel_driver.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from dynamixel_driver_pkg.DynamixelSync import DynamixelSync, CONTROL_TABLE, OPERATING_MODES
+from dynamixel_driver_pkg.DynamixelSyncDummy import DynamixelSyncDummy
+from plantwall_custom_interfaces.msg import MotorCmd, MotorState
+import numpy as np
+
+
+class DynamixelDriverNode(Node):
+    def __init__(self, node_name = 'dynamixel_driver'):
+        super().__init__(node_name)
+
+        # Motor initialization
+        self.motors = DynamixelSync()
+
+        self.motor_IDs =[1, 2, 3, 4, 11, 12]
+
+        self.create_subscription(MotorCmd, '/dynamixel_driver/motor_cmd', self.motor_write, 10)
+        self.motor_state_publisher = self.create_publisher(MotorState, '/dynamixel_driver/motor_state', 10)
+
+        # Main loop
+        self.loop_period = 0.1 # 10 Hz
+        self.control_timer = self.create_timer(self.loop_period, self.main_read)
+
+        self.get_logger().info(f"{node_name} Node has been started.")
+
+    def main_read(self):
+        motor_state_msg = MotorState()
+        motor_state_msg.motor_id = self.motor_IDs
+        motor_state_msg.operating_mode = self.motors.read(self.motor_IDs, CONTROL_TABLE.OPERATING_MODE)
+        motor_state_msg.homing_offset = self.motors.read(self.motor_IDs, CONTROL_TABLE.HOMING_OFFSET)
+        motor_state_msg.current_limit = self.motors.read(self.motor_IDs, CONTROL_TABLE.CURRENT_LIMIT)
+        motor_state_msg.velocity_limit = self.motors.read(self.motor_IDs, CONTROL_TABLE.VELOCITY_LIMIT)
+        motor_state_msg.torque_enable = self.motors.read(self.motor_IDs, CONTROL_TABLE.TORQUE_ENABLE)
+        motor_state_msg.goal_current = self.motors.read(self.motor_IDs, CONTROL_TABLE.GOAL_CURRENT)
+        motor_state_msg.goal_velocity = self.motors.read(self.motor_IDs, CONTROL_TABLE.GOAL_VELOCITY)
+        motor_state_msg.profile_velocity = self.motors.read(self.motor_IDs, CONTROL_TABLE.PROFILE_VELOCITY)
+        motor_state_msg.goal_position = self.motors.read(self.motor_IDs, CONTROL_TABLE.GOAL_POSITION)
+        motor_state_msg.moving = self.motors.read(self.motor_IDs, CONTROL_TABLE.MOVING)
+        motor_state_msg.present_load = self.motors.read(self.motor_IDs, CONTROL_TABLE.PRESENT_LOAD)
+        motor_state_msg.present_current = self.motors.read(self.motor_IDs, CONTROL_TABLE.PRESENT_CURRENT)
+        motor_state_msg.present_velocity = self.motors.read(self.motor_IDs, CONTROL_TABLE.PRESENT_VELOCITY)
+        motor_state_msg.present_position = self.motors.read(self.motor_IDs, CONTROL_TABLE.PRESENT_POSITION)
+
+        self.motor_state_publisher.publish(motor_state_msg)
+
+    def motor_write(self, msg):
+        self.motors.write(list(msg.motor_id), list(msg.value), self.get_control_table_by_address(msg.control_type_address))
+
+    def get_control_table_by_address(self, address: int) -> CONTROL_TABLE:
+        for control_type in CONTROL_TABLE:
+            if control_type.value[0] == address:
+                return control_type
+        self.motors.write(self.motor_IDs, [0,0,0,0,0,0], CONTROL_TABLE.TORQUE_ENABLE)
+        raise ValueError(f"No member in control table with adress {address}")
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = DynamixelDriverNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
